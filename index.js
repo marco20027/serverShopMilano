3dfbd2a354c376e9dfa916b1dba6100cb719cf8d
Marco <marco.campanale@par-tec.it>
2023-02-16 16:44:54 +0100

    push

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const fastify = require('fastify')({ logger: true })
 
+fastify.register(require('@fastify/mysql'), {
+  connectionString: 'mysql://root@localhost/shopmilano'
+})
+
+
 fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
 })
+
+fastify.post('/login', function (req, reply) {
+  try {
+      const { body: { email, password } } = req;
+      const sha1_password = String(CryptoJS.SHA1(password));
+      console.log(sha1_password)
+
+fastify.mysql.query(
+   'SELECT * FROM user where email=? AND password=? ', [email, sha1_password],
+   function onResult(err, result) {
+              console.log(err)
+              if (result.length > 0) {
+                  const token = fastify.jwt.sign({ id: result[0].id})
+                  reply.send({token})
+                  console.log(token)
+              } else {
+               reply.code(401).send({success: false, message: "Unauthorized access"})
+                  
+              }
+          }
+      )
+  } catch (err) {
+
+  }
+});
 
 
 const start = async () => {
