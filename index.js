45a2d4094b82dfac522591ead0d0f6890365a90b
Marco <marco.campanale@par-tec.it>
2023-02-17 17:16:43 +0100

    push

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//const CryptoJS = require("crypto-js");
 const fastify = require('fastify')({ logger: true })
 
 fastify.register(require('@fastify/mysql'), {
@@ -19,27 +20,39 @@ fastify.get('/', async (request, reply) => {
 fastify.post('/login', function (req, reply) {
   try {
       const { body: { email, password } } = req;
-      const sha1_password = String(CryptoJS.SHA1(password));
-      console.log(sha1_password)
+        fastify.mysql.query(
+          'SELECT * FROM login where email=? AND password=? ', [email, password],
+          function onResult(err, result) {
+                 reply.send(err || result)
+                   reply.code(401).send({success: false, message: "Unauthorized access"})
+                 }
+             )
+      }catch (err){
+        reply.code(400).send({success: false, message: "Unauthorized access"})
+      } 
+});
 
-fastify.mysql.query(
-   'SELECT * FROM user where email=? AND password=? ', [email, sha1_password],
-   function onResult(err, result) {
-              console.log(err)
-              if (result.length > 0) {
-                  const token = fastify.jwt.sign({ id: result[0].id})
-                  reply.send({token})
-                  console.log(token)
-              } else {
-               reply.code(401).send({success: false, message: "Unauthorized access"})
-                  
-              }
+
+fastify.route({
+  method: 'POST',
+  url: '/registrazione',
+
+  handler: function (req, reply) {
+      const registrazione = {
+          nome : req.body.nome,
+          cognome: req.body.cognome,
+          email: req.body.email,
+          password : req.body.password,
+      }
+      console.log(req.user)
+      fastify.mysql.query(
+          'INSERT INTO registrazione SET ? ', [registrazione],
+          function onResult(err, result) {
+              reply.send(err || result)
           }
       )
-  } catch (err) {
-
   }
-});
+})
 
 
 const start = async () => {
